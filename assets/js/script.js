2161d2951dba8a6cf4cb9e44b56c901d44d18fc3
MartinFLL <martin@oyl.cl>
2025-11-27 01:11:47 -0300

    Arreglos minimos en la responsividad del sitio y adición del lugar, dropdown e insignia de verificación

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+
+  // Inicializar Tooltips de Bootstrap
+  const tooltipTriggerList = document.querySelectorAll(
+    '[data-bs-toggle="tooltip"]'
+  );
+  const tooltipList = [...tooltipTriggerList].map(
+    (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+  );
+
+});
